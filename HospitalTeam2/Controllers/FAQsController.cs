49404f47626f4efa6576865800381f536877a7a3
divinshah <n01315099@humbermail.ca>
2019-04-05 15:55:03 -0400

    new feature

---
+++ HospitalTeam2/Controllers/FAQsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HospitalTeam2.Data;
+using HospitalTeam2.Models;
+
+namespace HospitalTeam2.Controllers
+{
+    public class FAQsController : Controller
+    {
+        private readonly HospitalCMSContext _context;
+
+        public FAQsController(HospitalCMSContext context)
+        {
+            _context = context;
+        }
+
+        // GET: FAQs
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.FAQs.ToListAsync());
+        }
+
+        // GET: FAQs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fAQ = await _context.FAQs
+                .SingleOrDefaultAsync(m => m.FaqID == id);
+            if (fAQ == null)
+            {
+                return NotFound();
+            }
+
+            return View(fAQ);
+        }
+
+        // GET: FAQs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FAQs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FaqID,FaqQues,FaqAns")] FAQ fAQ)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(fAQ);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(fAQ);
+        }
+
+        // GET: FAQs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fAQ = await _context.FAQs.SingleOrDefaultAsync(m => m.FaqID == id);
+            if (fAQ == null)
+            {
+                return NotFound();
+            }
+            return View(fAQ);
+        }
+
+        // POST: FAQs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("FaqID,FaqQues,FaqAns")] FAQ fAQ)
+        {
+            if (id != fAQ.FaqID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(fAQ);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FAQExists(fAQ.FaqID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(fAQ);
+        }
+
+        // GET: FAQs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fAQ = await _context.FAQs
+                .SingleOrDefaultAsync(m => m.FaqID == id);
+            if (fAQ == null)
+            {
+                return NotFound();
+            }
+
+            return View(fAQ);
+        }
+
+        // POST: FAQs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var fAQ = await _context.FAQs.SingleOrDefaultAsync(m => m.FaqID == id);
+            _context.FAQs.Remove(fAQ);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FAQExists(int id)
+        {
+            return _context.FAQs.Any(e => e.FaqID == id);
+        }
+    }
+}
---
+++ HospitalTeam2/Controllers/ParkingsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HospitalTeam2.Data;
+using HospitalTeam2.Models;
+
+namespace HospitalTeam2.Controllers
+{
+    public class ParkingsController : Controller
+    {
+        private readonly HospitalCMSContext _context;
+
+        public ParkingsController(HospitalCMSContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Parkings
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Parkings.ToListAsync());
+        }
+
+        // GET: Parkings/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var parking = await _context.Parkings
+                .SingleOrDefaultAsync(m => m.ParkingID == id);
+            if (parking == null)
+            {
+                return NotFound();
+            }
+
+            return View(parking);
+        }
+
+        // GET: Parkings/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Parkings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ParkingID,VisitorName,VisitoCarNo,ParkingPurpose,ParkingContact")] Parking parking)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(parking);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(parking);
+        }
+
+        // GET: Parkings/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var parking = await _context.Parkings.SingleOrDefaultAsync(m => m.ParkingID == id);
+            if (parking == null)
+            {
+                return NotFound();
+            }
+            return View(parking);
+        }
+
+        // POST: Parkings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ParkingID,VisitorName,VisitoCarNo,ParkingPurpose,ParkingContact")] Parking parking)
+        {
+            if (id != parking.ParkingID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(parking);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ParkingExists(parking.ParkingID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(parking);
+        }
+
+        // GET: Parkings/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var parking = await _context.Parkings
+                .SingleOrDefaultAsync(m => m.ParkingID == id);
+            if (parking == null)
+            {
+                return NotFound();
+            }
+
+            return View(parking);
+        }
+
+        // POST: Parkings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var parking = await _context.Parkings.SingleOrDefaultAsync(m => m.ParkingID == id);
+            _context.Parkings.Remove(parking);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ParkingExists(int id)
+        {
+            return _context.Parkings.Any(e => e.ParkingID == id);
+        }
+    }
+}
