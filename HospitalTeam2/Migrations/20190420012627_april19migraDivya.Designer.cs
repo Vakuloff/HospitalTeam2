6e80ddc7d27c0166bd2e58473f587b7171dadd37
divinshah <n01315099@humbermail.ca>
2019-04-19 21:47:21 -0400

    working image upload donors

--- HospitalTeam2/Migrations/20190420012627_april19migraDivya.Designer.cs
+++ HospitalTeam2/Migrations/20190420012627_april19migraDivya.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace HospitalTeam2.Migrations
 {
     [DbContext(typeof(HospitalCMSContext))]
-    partial class HospitalCMSContextModelSnapshot : ModelSnapshot
+    [Migration("20190420012627_april19migraDivya")]
+    partial class april19migraDivya
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ HospitalTeam2/Migrations/20190420012627_april19migraDivya.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace HospitalTeam2.Migrations
+{
+    public partial class april19migraDivya : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //migrationBuilder.AddColumn<int>(
+            //    name: "HasPic",
+            //    table: "Donors",
+            //    nullable: false,
+            //    defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasPic",
+                table: "Donors");
+        }
+    }
+}
